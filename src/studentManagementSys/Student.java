430f8958c0aa7fb2413503683dfe374b225c812d
MohamedJamiDev <mohamed.sul02@gmail.com>
2025-02-18 16:38:05 -0600

    Practicing using ArrayList

---
+++ src/studentManagementSys/Student.java
@@ -0,0 +1,41 @@
+package studentManagementSys;
+
+/* 📌 Project Features:
+ 1.	Add a new student (name, ID, grade)
+ 2.	Display all students
+ 3.	Count total students using static
+ 4.	Use final for student ID*/
+
+
+public class Student {
+
+   private static int counter = 10;
+   private final int studentId;
+
+   private String studentName;
+   private double studentGrade;
+
+
+
+    Student(String studentName, double studentGrade){
+
+        this.studentName = studentName;
+        this.studentGrade = studentGrade;
+        this.studentId =  ++counter;
+    }
+
+
+    public void displayStudentInfo(){
+
+        System.out.println("Student Name: "+ studentName);
+        System.out.println("Student Grade "+ studentGrade);
+        System.out.println("Student Id: "+ studentId);
+    }
+
+    public static int getTotalStudents(){
+        return counter;
+    }
+
+
+    }
+
